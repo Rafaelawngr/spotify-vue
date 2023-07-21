a79723f6fbd967627d482f142a465ecca3b698ef
Rafaela Ellwanger <rafaela.ellwanger@hotmail.com>
2023-07-21 15:54:45 -0300

    esqueleto página + add icons

--- spotify-vue/src/components/homePageComponent.vue
+++ spotify-vue/src/components/homePageComponent.vue
@@ -3,9 +3,21 @@
 </script>
 
 <template>
+  <main>
+    <HeaderComponent/>
 
+  </main>
 </template>
 
 <style scoped>
+
+main {
+  display: flex;
+  width: 100%;
+  padding: 10px;
+  grid-area: main;
+  border-radius: 10px;
+  background: linear-gradient(360deg, #121212 14.84%, #232323 70.76%, #404040 100%);
+}
 
 </style>
---
+++ spotify-vue/src/components/sideMenuLibraryComponent.vue
@@ -0,0 +1,57 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="menu-library">
+    <h1 class="menu-library-title"><span class="material-symbols-outlined">
+import_contacts
+</span>Biblioteca</h1>
+    <div class="menu-library-items">
+      <button>Músicas curtidas</button>
+      <button>Radar de Novidades</button>
+      <button>Tocadas Recentemente</button>
+      <button>Álbuns</button>
+      <button>Artistas</button>
+      <button>Podcasts</button>
+    </div>
+  </div>
+  <div class="menu-playlists">
+  </div>
+</template>
+
+<style scoped>
+
+.menu-library {
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  padding: 10px;
+  background: var(--sideMenuColor);
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+  grid-area: side2;
+}
+
+.menu-library-title {
+  display: flex;
+  align-items: center;
+  gap: 15px;
+  padding: 10px;
+  font-size: 1rem;
+  color: var(--fontColorSmall);
+}
+
+.menu-library-items button {
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  font-size: .8rem;
+  font-weight: 600;
+  color: var(--fontColorSmall);
+  background: none;
+  border: none;
+}
+
+</style>
